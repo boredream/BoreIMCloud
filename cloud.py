63ee3f11a666324b18751d1d4b3f0fb31cdf201c
boredream <48262906@qq.com>
2017-07-14 15:05:15 +0800

    添加好友使用Pointer关联表； 新增获取我的好友功能

--- cloud.py
+++ cloud.py
@@ -84,35 +84,68 @@ def imlogin(**params):
 
 
 @engine.define
+def getFriends(**params):
+    user = engine.current.user
+    srcUserId = user.get('objectId')
+    srcUser = leancloud.User.create_without_data(srcUserId)
+
+    query_src = leancloud.Query(query_class='FriendRelation') \
+        .equal_to(key='srcUser', value=srcUser)
+    query_tar = leancloud.Query(query_class='FriendRelation') \
+        .equal_to(key='tarUser', value=srcUser)
+    query_relation = leancloud.Query(query_class='FriendRelation') \
+        .equal_to(key='relation', value=1)
+    query_id = leancloud.Query(query_class='FriendRelation') \
+        .or_(query_src, query_tar)
+    query = leancloud.Query(query_class='FriendRelation') \
+        .and_(query_id, query_relation)
+
+    usres = []
+    try:
+        for result in query.find():
+            src = result.dump().get('srcUser')
+            tar = result.dump().get('tarUser')
+            if src.get('objectId') == srcUserId:
+                usres.append(tar)
+            else:
+                usres.append(src)
+    except LeanCloudError:
+        print 'no friends'
+
+    return usres
+
+
+@engine.define
 def friendRequest(**params):
     user = engine.current.user
     srcUserId = user.get('objectId')
-    targetUserId = params.get('userId')
+    tarUserId = params.get('userId')
+
+    srcUser = leancloud.User.create_without_data(srcUserId)
+    tarUser = leancloud.User.create_without_data(tarUserId)
 
     # src -> target
-    query_src = leancloud.Query(query_class='FriendRelation')
-    query_tar = leancloud.Query(query_class='FriendRelation')
-    query = leancloud.Query(query_class='FriendRelation')
-
-    query_src.equal_to(key='srcUserId', value=srcUserId)
-    query_tar.equal_to(key='targetUserId', value=targetUserId)
+    query_src = leancloud.Query(query_class='FriendRelation') \
+        .equal_to(key='srcUser', value=srcUser)
+    query_tar = leancloud.Query(query_class='FriendRelation') \
+        .equal_to(key='tarUser', value=tarUser)
     result = None
     try:
-        result = query.and_(query_src, query_tar).first()
+        result = leancloud.Query(query_class='FriendRelation') \
+            .and_(query_src, query_tar).first()
     except LeanCloudError, e:
         if e.code != 101:
             raise e
 
     # target -> src
-    query_src_rev = leancloud.Query(query_class='FriendRelation')
-    query_tar_rev = leancloud.Query(query_class='FriendRelation')
-    query_rev = leancloud.Query(query_class='FriendRelation')
-
-    query_src_rev.equal_to(key='srcUserId', value=targetUserId)
-    query_tar_rev.equal_to(key='targetUserId', value=srcUserId)
+    query_src_rev = leancloud.Query(query_class='FriendRelation') \
+        .equal_to(key='srcUser', value=tarUser)
+    query_tar_rev = leancloud.Query(query_class='FriendRelation') \
+        .equal_to(key='tarUser', value=srcUser)
     result_rev = None
     try:
-        result_rev = query_rev.and_(query_src_rev, query_tar_rev).first()
+        result_rev = leancloud.Query(query_class='FriendRelation') \
+            .and_(query_src_rev, query_tar_rev).first()
     except LeanCloudError, e:
         if e.code != 101:
             raise e
@@ -120,41 +153,38 @@ def friendRequest(**params):
     if (result and result.get('relation') == 1) or \
             (result_rev and result_rev.get('relation') == 1):
         # 如果已经是好友，则返回已添加提示
-        print '[src=' + srcUserId + '] and [tar=' + targetUserId + '] is already friend'
+        print '[src=' + srcUserId + '] and [tar=' + tarUserId + '] is already friend'
         raise LeanEngineError(u'已经是好友了，无需重复添加')
 
     if result and result.get('relation') == -1:
         # 如果src曾经向tar提交过申请，也视为成功申请
         # 但是数据库中不再重复添加好友关系
-        print '[src='+srcUserId+'] has requested friend to [tar='+targetUserId+'] before'
+        print '[src=' + srcUserId + '] has requested friend to [tar=' + tarUserId + '] before'
         return
 
     if result_rev and result_rev.get('relation') == -1:
         # 如果是tar已经向src提交过申请，则直接relation=1双方改为好友
         result_rev.set('relation', 1)
         result_rev.save()
-        print '[tar='+targetUserId+'] has requested friend to [src='+srcUserId+'], be friend now'
+        print '[tar=' + tarUserId + '] has requested friend to [src=' + srcUserId + '], be friend now'
         return
 
     # 如果双方没关系，则新建一条src申请加tar为好友的关系数据
     relation = leancloud.Object().create(class_name='FriendRelation')
-    relation.set('srcUserId', srcUserId)
-    relation.set('targetUserId', targetUserId)
+    relation.set('srcUser', srcUser)
+    relation.set('tarUser', tarUser)
     relation.save()
-    print '[src='+srcUserId+'] request friend to [tar='+targetUserId+']'
+    print '[src=' + srcUserId + '] request friend to [tar=' + tarUserId + ']'
 
     # 同时向tar发送一条IM系统消息“src申请添加您为好友”
     message = u'申请添加您为好友'
     rcloud.Message.PublishSystem(
         fromUserId=srcUserId,
-        toUserId={targetUserId},
+        toUserId={tarUserId},
         objectName='RC:TxtMsg',
         content="{\"content\":\""+message+"\",\"extra\":\"helloExtra\"}",
         pushContent='thisisapush',
         pushData="{\"pushData\":\""+message+"\"}",
         isPersisted='0',
         isCounted='0')
-
-    print 'friend request '+srcUserId+' -> '+targetUserId
-
 
--- lc_test.py
+++ lc_test.py
@@ -11,35 +11,36 @@ MASTER_KEY = "j7Ncx5DPSokvrSQAuShvJ788"
 leancloud.init(APP_ID, app_key=APP_KEY, master_key=MASTER_KEY)
 leancloud.use_master_key(True)
 
-srcUserId = '22'
-targetUserId = '12'
+srcUserId = '5949e37fda2f6000679a9b74'
+tarUserId = '1'
 
 
 def test_friend_request():
-    # src -> target
-    query_src = leancloud.Query(query_class='FriendRelation')
-    query_tar = leancloud.Query(query_class='FriendRelation')
-    query = leancloud.Query(query_class='FriendRelation')
+    srcUser = leancloud.User.create_without_data(srcUserId)
+    tarUser = leancloud.User.create_without_data(tarUserId)
 
-    query_src.equal_to(key='srcUserId', value=srcUserId)
-    query_tar.equal_to(key='targetUserId', value=targetUserId)
+    # src -> target
+    query_src = leancloud.Query(query_class='FriendRelation')\
+        .equal_to(key='srcUser', value=srcUser)
+    query_tar = leancloud.Query(query_class='FriendRelation')\
+        .equal_to(key='tarUser', value=tarUser)
     result = None
     try:
-        result = query.and_(query_src, query_tar).first()
+        result = leancloud.Query(query_class='FriendRelation')\
+            .and_(query_src, query_tar).first()
     except LeanCloudError, e:
         if e.code != 101:
             raise e
 
     # target -> src
-    query_src_rev = leancloud.Query(query_class='FriendRelation')
-    query_tar_rev = leancloud.Query(query_class='FriendRelation')
-    query_rev = leancloud.Query(query_class='FriendRelation')
-
-    query_src_rev.equal_to(key='srcUserId', value=targetUserId)
-    query_tar_rev.equal_to(key='targetUserId', value=srcUserId)
+    query_src_rev = leancloud.Query(query_class='FriendRelation')\
+        .equal_to(key='srcUser', value=tarUser)
+    query_tar_rev = leancloud.Query(query_class='FriendRelation')\
+        .equal_to(key='tarUser', value=srcUser)
     result_rev = None
     try:
-        result_rev = query_rev.and_(query_src_rev, query_tar_rev).first()
+        result_rev = leancloud.Query(query_class='FriendRelation')\
+            .and_(query_src_rev, query_tar_rev).first()
     except LeanCloudError, e:
         if e.code != 101:
             raise e
@@ -47,34 +48,63 @@ def test_friend_request():
     if (result and result.get('relation') == 1) or \
             (result_rev and result_rev.get('relation') == 1):
         # 如果已经是好友，则返回已添加提示
-        print '[src=' + srcUserId + '] and [tar=' + targetUserId + '] is already friend'
+        print '[src=' + srcUserId + '] and [tar=' + tarUserId + '] is already friend'
         raise LeanEngineError(u'已经是好友了，无需重复添加')
 
     if result and result.get('relation') == -1:
         # 如果src曾经向tar提交过申请，也视为成功申请
         # 但是数据库中不再重复添加好友关系
-        print '[src='+srcUserId+'] has requested friend to [tar='+targetUserId+'] before'
+        print '[src='+srcUserId+'] has requested friend to [tar='+tarUserId+'] before'
         return
 
     if result_rev and result_rev.get('relation') == -1:
         # 如果是tar已经向src提交过申请，则直接relation=1双方改为好友
         result_rev.set('relation', 1)
         result_rev.save()
-        print '[tar='+targetUserId+'] has requested friend to [src='+srcUserId+'], be friend now'
+        print '[tar='+tarUserId+'] has requested friend to [src='+srcUserId+'], be friend now'
         return
 
     # 如果双方没关系，则新建一条src申请加tar为好友的关系数据
     relation = leancloud.Object().create(class_name='FriendRelation')
-    relation.set('srcUserId', srcUserId)
-    relation.set('targetUserId', targetUserId)
+    relation.set('srcUser', srcUser)
+    relation.set('tarUser', tarUser)
     relation.save()
-    print '[src='+srcUserId+'] request friend to [tar='+targetUserId+']'
+    print '[src='+srcUserId+'] request friend to [tar='+tarUserId+']'
 
     # 同时向tar发送一条IM系统消息“src申请添加您为好友”
 
 
+def get_friends():
+    srcUser = leancloud.User.create_without_data(srcUserId)
+
+    query_src = leancloud.Query(query_class='FriendRelation')\
+        .equal_to(key='srcUser', value=srcUser)
+    query_tar = leancloud.Query(query_class='FriendRelation')\
+        .equal_to(key='tarUser', value=srcUser)
+    query_relation = leancloud.Query(query_class='FriendRelation')\
+        .equal_to(key='relation', value=1)
+    query_id = leancloud.Query(query_class='FriendRelation')\
+        .or_(query_src, query_tar)
+    query = leancloud.Query(query_class='FriendRelation') \
+        .and_(query_id, query_relation)
+
+    usres = []
+    try:
+        for result in query.find():
+            src = result.dump().get('srcUser')
+            tar = result.dump().get('tarUser')
+            if src.get('objectId') == srcUserId:
+                usres.append(tar)
+            else:
+                usres.append(src)
+    except LeanCloudError:
+        print 'no friends'
+
+    return usres
+
+
 if __name__ == '__main__':
-    test_friend_request()
+    get_friends()
 
 
 
